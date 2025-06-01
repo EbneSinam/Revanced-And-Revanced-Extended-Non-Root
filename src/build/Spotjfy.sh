e2bf7de4cc42d34655d0fc8582e6ce51682bc4e1
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-01 21:35:18 +0330

    Update Spotjfy.sh

--- src/build/Spotjfy.sh
+++ src/build/Spotjfy.sh
@@ -6,7 +6,7 @@ source src/build/utils.sh
 dl_gh "revanced-patches" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
 # Patch Spotjfy Arm64-v8a
-get_patches_key "Spotjfy-revanced"
+get_patches_key "Spotjfy-anddea"
 j="i"
 get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
 patch "spotjfy-arm64-v8a" "anddea"
