847155e31d055df2245dad8c9f55f4f32fa34e6f
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-01 21:04:14 +0330

    Update Anddea-Revanced-Extended-Beta.sh

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -5,11 +5,6 @@ source src/build/utils.sh
 dl_gh "revanced-patches" "anddea" "prerelease"
 dl_gh "revanced-cli" "inotia00" "latest"
 
-# Patch YouTube:
-get_patches_key "youtube-rve-anddea"
-get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-split_editor "youtube-beta" "youtube-beta"
-patch "youtube-beta" "anddea" "inotia"
 # Patch Youtube Arm64-v8a
 get_patches_key "youtube-rve-anddea"
 split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
@@ -20,3 +15,9 @@ patch "youtube-beta-arm64-v8a" "anddea" "inotia"
 get_patches_key "youtube-music-rve-anddea"
 get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-beta-arm64-v8a" "anddea" "inotia"
+
+# Patch Spotjfy Arm64-v8a
+get_patches_key "Spotjfy-revanced"
+j="i"
+get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+patch "spotjfy-arm64-v8a" "anddea"
