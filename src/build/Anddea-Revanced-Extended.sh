6a4dfe3e5edf5228608dc9a9ebbd54dad56f67d1
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-01 21:36:49 +0330

    Update Anddea-Revanced-Extended.sh

--- src/build/Anddea-Revanced-Extended.sh
+++ src/build/Anddea-Revanced-Extended.sh
@@ -10,8 +10,7 @@ get_patches_key "youtube-rve-anddea"
 split_editor "youtube-stable" "youtube-stable-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-stable-arm64-v8a" "anddea" "inotia"
 
-# Patch YouTube Music:
-# Arm64-v8a
+# Patch YouTube Music Arm64-v8a
 get_patches_key "youtube-music-rve-anddea"
 get_apk "com.google.android.apps.youtube.music" "youtube-music-stable-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-stable-arm64-v8a" "anddea" "inotia"
@@ -21,3 +20,8 @@ get_patches_key "Spotjfy-revanced"
 j="i"
 get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
 patch "spotjfy-arm64-v8a" "anddea"
+
+# Patch Reddit Arm64-v8a:
+split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+get_patches_key "reddit-rve"
+patch "reddit-arm64-v8a" "anddea" "inotia"
