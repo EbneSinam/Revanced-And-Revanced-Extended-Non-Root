068a65fb3b89e067ef1dba02d55eea83503f9da2
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-03 17:35:26 +0330

    Add files via upload

---
+++ src/build/Revanced-Beta.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Revanced build
+source ./src/build/utils.sh
+# Download requirements
+revanced_dl(){
+	dl_gh "revanced-patches" "revanced" "prerelease"
+ 	dl_gh "revanced-cli" "revanced" "latest"
+}
+1() {
+	revanced_dl
+	# Patch Spotjfy Arm64-v8a
+	get_patches_key "Spotjfy-revanced"
+	j="i"
+	get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+	patch "spotjfy-beta-arm64-v8a" "revanced"
+}
+case "$1" in
+    1)
+        1
+        ;;
+esac
