e34fc230ac1e0cdb8b1c14f526bc49f88bcad2f5
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-03 14:05:53 +0330

    Update Anddea-Revanced-Extended.sh

--- src/build/Anddea-Revanced-Extended.sh
+++ src/build/Anddea-Revanced-Extended.sh
@@ -20,6 +20,7 @@ patch "youtube-music-stable-arm64-v8a" "anddea" "inotia"
 get_patches_key "Spotjfy-anddea"
 j="i"
 get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+split_editor "spot"$j"fy-arm64-v8a" "spot"$j"fy"
 patch "spotjfy-arm64-v8a" "anddea"
 
 # Patch Reddit Arm64-v8a
