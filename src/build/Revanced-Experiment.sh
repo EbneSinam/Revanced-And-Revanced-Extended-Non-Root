a46e393bb9125b838a9f6a25276d412a9778cd04
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-17 12:01:14 +0330

    Update Revanced-Experiment.sh

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -12,7 +12,7 @@ dl_gh "revanced-cli" "revanced" "latest"
 
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
-#version="384.0.0.46.83"
+version="378.0.0.52.68"
 get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi" #"Bundle"
 #split_editor "instagram" "instagram-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "instagram-arm64-v8a" "revanced-experiments"
