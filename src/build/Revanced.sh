7d1a2e705005bd79707ed09e95281271e9a825cd
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-06-22 19:05:48 +0330

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -10,7 +10,7 @@ revanced_dl(){
 	# Patch Spotjfy Arm64-v8a
 	get_patches_key "Spotjfy-revanced"
 	j="i"
-        version="9.0.52.505"
+        version="9.0.54.518"
 	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" #"Bundle_extract"
 	#split_editor "spotjfy" "spotjfy-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "spotjfy-arm64-v8a" "revanced"
