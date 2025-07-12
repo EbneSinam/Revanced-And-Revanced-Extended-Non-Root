6984ab3e1513087c4917f388a2d91fd4172885dd
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-07-12 18:53:46 +0330

    Update Revanced-Beta.sh

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # Revanced build
-    # این خط برای مطابقت با نام پوشه شما تغییر کرده است
-    get_patches_key "Spotjfy-revanced"
-	
- 	version="9.0.62.603"
-    
-	get_apk "com.spot${j}fy.music" "spotjfy-beta-arm64-v8a" "spot${j}fy-music-podcasts" "spot${j}fy-ab/spot${j}fy-music-podcasts/spot${j}fy-music-and-podcasts" "nodpi"
-    
-    patch "spotjfy-beta-arm64-v8a" "revanced"
+source ./src/build/utils.sh
+# Download requirements
+revanced_dl(){
+	dl_gh "revanced-patches" "revanced" "prerelease"
+ 	dl_gh "revanced-cli" "revanced" "latest"
 }
-
+1() {
+	revanced_dl
+	# Patch Spotjfy Arm64-v8a
+	get_patches_key "Spotjfy-revanced"
+	j="i"
+ 	version="9.0.62.603"
+	get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle" "Bundle_extract"
+ 	split_editor "spotjfy-beta" "spotjfy-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+	patch "spotjfy-beta-arm64-v8a" "revanced"
+}
 case "$1" in
     1)
-        patch_spotify
+        1
         ;;
 esac
