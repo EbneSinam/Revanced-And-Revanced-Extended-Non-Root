e67003d26b796675e499fc18d530dcc8e93e0525
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-07-18 00:13:37 +0330

    Update Revanced-Beta.sh

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -11,9 +11,9 @@ revanced_dl(){
 	# Patch Spotjfy Arm64-v8a
 	get_patches_key "Spotjfy-revanced"
 	j="i"
- 	version="9.0.64.106"
-	get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" #"Bundle_extract"
- 	#split_editor "spotjfy-beta" "spotjfy-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+ 	version="9.0.62.622"
+	get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle_extract"
+ 	split_editor "spotjfy-beta" "spotjfy-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "spotjfy-beta-arm64-v8a" "revanced"
 }
 case "$1" in
