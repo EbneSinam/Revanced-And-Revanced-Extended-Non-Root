e68db48fe2e4dac385ea4c7e0bc702d6fd259cd8
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-07-19 01:13:50 +0330

    Update Piko.sh

--- src/build/Piko.sh
+++ src/build/Piko.sh
@@ -2,7 +2,7 @@
 # Twitter Piko
 source src/build/utils.sh
 
-# Patch Twitter Piko:
+# Patch Twitter Piko Arm64-v8a:
 patch_piko () {
 	dl_gh "revanced-cli" "revanced" "v4.6.0"
 	get_patches_key "twitter-piko"
@@ -14,11 +14,7 @@ patch_piko () {
 	fi
 	dl_gh "piko revanced-integrations" "crimera" "$v"
 	get_apk "com.twitter.android" "twitter-$apk_name" "twitter" "x-corp/twitter/x-previously-twitter" "Bundle_extract"
-	split_editor "twitter-$apk_name" "twitter-$apk_name"
-	patch "twitter-$apk_name" "piko"
-	# Patch Twitter Piko Arm64-v8a:
-	get_patches_key "twitter-piko"
 	split_editor "twitter-$apk_name" "twitter-arm64-v8a-$apk_name" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-	patch "twitter-arm64-v8a-$apk_name" "piko"
+	patch "twitter-arm64-v8a-$apk_name" "piko" 
 }
 patch_piko $1
