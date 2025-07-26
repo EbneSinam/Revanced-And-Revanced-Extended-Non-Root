855cdb6423f4726d6d9a13a175d2348c49ca7722
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-07-26 04:43:41 +0330

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -14,6 +14,12 @@ revanced_dl(){
 	get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" #"Bundle_extract"
 	#split_editor "spotjfy" "spotjfy-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 	patch "spotjfy-arm64-v8a" "revanced"
+
+	# Patch Twitch Arm64-v8a:
+	get_patches_key "twitch"
+	get_apk "tv.twitch.android.app" "twitch-arm64-v8a" "twitch" "twitch-interactive-inc/twitch/twitch" "Bundle_extract"
+	split_editor "twitch" "twitch-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+	patch "twitch-arm64-v8a" "revanced"
 }
 case "$1" in
     1)
