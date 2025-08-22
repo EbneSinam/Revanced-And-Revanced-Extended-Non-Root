7239d086066230b15b2f637189a0dc810c8bde6e
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-08-22 22:35:13 +0330

    Update Anddea-Revanced-Extended-Beta.sh

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -8,7 +8,7 @@ dl_gh "revanced-cli" "inotia00" "latest"
 # Patch Spotjfy Arm64-v8a
 get_patches_key "Spotjfy-anddea"
 j="i"
-version="9.0.66.141"
+version="9.0.72.967"
 get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle_extract"
 split_editor "spotjfy-beta" "spotjfy-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "spotjfy-beta-arm64-v8a" "anddea"
