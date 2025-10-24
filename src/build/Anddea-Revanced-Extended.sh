d6a35d2ee01866fb303071a1d6656c584c7baba6
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-10-25 01:04:05 +0330

    Update Anddea-Revanced-Extended.sh

--- src/build/Anddea-Revanced-Extended.sh
+++ src/build/Anddea-Revanced-Extended.sh
@@ -16,6 +16,12 @@ get_patches_key "youtube-music-rve-anddea"
 get_apk "com.google.android.apps.youtube.music" "youtube-music-stable-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-stable-arm64-v8a" "anddea" "inotia"
 
+# Patch Reddit Arm64-v8a
+get_patches_key "reddit-anddea"
+get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
+split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+patch "reddit-arm64-v8a" "anddea" "inotia"
+
 # Patch Spotjfy Arm64-v8a
 # get_patches_key "Spotjfy-anddea"
 # j="i"
