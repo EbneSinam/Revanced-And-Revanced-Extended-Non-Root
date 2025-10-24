8c1b9e10b2ba328a986735cb208750fe7452460a
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2025-10-25 00:49:41 +0330

    Update Piko-Beta.sh

--- src/build/Piko-Beta.sh
+++ src/build/Piko-Beta.sh
@@ -7,6 +7,6 @@ dl_gh "piko" "crimera" "prerelease"
 
 # Patch Twitter Piko Arm64-v8a:
 get_patches_key "twitter-piko"
-get_apk "com.twitter.android" "twitter-beta" "twitter" "x-corp/twitter/x-formerly-twitter" "Bundle_extract"
+get_apk "com.twitter.android" "twitter-beta" "twitter" "x-corp/twitter/x" "Bundle_extract"
 split_editor "twitter-beta" "twitter-arm64-v8a-beta" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
 patch "twitter-arm64-v8a-beta" "piko"
