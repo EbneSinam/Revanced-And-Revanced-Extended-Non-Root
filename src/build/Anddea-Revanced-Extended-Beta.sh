e03e74bcaac98d514bc6fd84f4747f2f7df627d9
Sina.M <125854733+EbneSinam@users.noreply.github.com>
2026-02-19 23:58:49 +0330

    Update Anddea-Revanced-Extended-Beta.sh

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -13,7 +13,6 @@ anddea_dl(){
 	# Patch YouTube:
 	get_patches_key "youtube-rve-anddea"
 	get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
-	patch "youtube-beta" "anddea" "morphe"
 	# Remove unused architectures
 	for i in {0..3}; do
 		apk_editor "youtube-beta" "${archs[i]}" ${libs[i]}
@@ -21,15 +20,6 @@ anddea_dl(){
 	# Patch Youtube Arm64-v8a
 	get_patches_key "youtube-rve-anddea"
 	patch "youtube-beta-arm64-v8a" "anddea" "morphe"
-	# Patch Youtube Armeabi-v7a
-	get_patches_key "youtube-rve-anddea"
-	patch "youtube-beta-armeabi-v7a" "anddea" "morphe"
-	# Patch Youtube x86
-	get_patches_key "youtube-rve-anddea"
-	patch "youtube-beta-x86" "anddea" "morphe"
-	# Patch Youtube x86_64
-	get_patches_key "youtube-rve-anddea"
-	patch "youtube-beta-x86_64" "anddea" "morphe"
 }
 2() {
 	anddea_dl
@@ -38,18 +28,6 @@ anddea_dl(){
 	get_patches_key "youtube-music-rve-anddea"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 	patch "youtube-music-beta-arm64-v8a" "anddea" "morphe"
-	# Armeabi-v7a
-	get_patches_key "youtube-music-rve-anddea"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
-	patch "youtube-music-beta-armeabi-v7a" "anddea" "morphe"
-	# x86_64
-	get_patches_key "youtube-music-rve-anddea"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
-	patch "youtube-music-beta-x86_64" "anddea" "morphe"
-	# x86
-	get_patches_key "youtube-music-rve-anddea"
-	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
-	patch "youtube-music-beta-x86" "anddea" "morphe"
 }
 case "$1" in
     1)
